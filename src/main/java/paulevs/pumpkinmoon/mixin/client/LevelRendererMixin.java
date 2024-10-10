62ff44461954b3559afeb6b81197e1ef95ca0c79
paulevsGitch <paulevs@yandex.ru>
2024-10-10 16:11:55 +0300

    Fixed dimension check, moon texture rotation

--- src/main/java/paulevs/pumpkinmoon/mixin/client/LevelRendererMixin.java
+++ src/main/java/paulevs/pumpkinmoon/mixin/client/LevelRendererMixin.java
@@ -8,11 +8,17 @@ import net.minecraft.level.Level;
 import net.modificationstation.stationapi.api.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import paulevs.pumpkinmoon.PumpkinMoon;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
+	@Unique private static final String[] PUMPKINMOON_MOON_TEXTURES = new String[] {
+		"/assets/pumpkinmoon/textures/moon.png",
+		"/assets/pumpkinmoon/textures/moon_rotated.png"
+	};
+	
 	@Shadow private TextureManager textureManager;
 	@Shadow private Level level;
 	
@@ -23,8 +29,9 @@ public class LevelRendererMixin {
 		ordinal = 1
 	))
 	private void pumpkinmoon_changeMoonTexture(int target, int texture, Operation<Void> original) {
-		if (PumpkinMoon.isPumpkinMoon && level.dimension.id != 0) {
-			texture = textureManager.getTextureId("/assets/pumpkinmoon/textures/moon.png");
+		if (PumpkinMoon.isPumpkinMoon && level.dimension.id == 0) {
+			int index = (int) (level.getLevelTime() % 24000L) > 18000 ? 1 : 0;
+			texture = textureManager.getTextureId(PUMPKINMOON_MOON_TEXTURES[index]);
 		}
 		original.call(target, texture);
 	}
@@ -46,7 +53,7 @@ public class LevelRendererMixin {
 		ordinal = 1
 	))
 	private void pumpkinmoon_changeStarsColor(float red, float green, float blue, float alpha, Operation<Void> original) {
-		if (PumpkinMoon.isPumpkinMoon && level.dimension.id != 0) {
+		if (PumpkinMoon.isPumpkinMoon && level.dimension.id == 0) {
 			red = 1.0F;
 			green = 0.843F;
 			blue = 0.0F;
@@ -61,7 +68,7 @@ public class LevelRendererMixin {
 		ordinal = 1
 	))
 	private void pumpkinmoon_changeSkyColor(float red, float green, float blue, Operation<Void> original) {
-		if (PumpkinMoon.isPumpkinMoon && level.dimension.id != 0) {
+		if (PumpkinMoon.isPumpkinMoon && level.dimension.id == 0) {
 			red = MathHelper.lerp(PumpkinMoon.effectIntensity, red, 1.0F);
 			green = MathHelper.lerp(PumpkinMoon.effectIntensity, green, 0.0F);
 			blue = MathHelper.lerp(PumpkinMoon.effectIntensity, blue, 0.0F);
